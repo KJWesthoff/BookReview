bbea4c9174ba8a1c650e11d392c113428e72976c
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-01-14 12:29:09 -0800

    Get routes with auth in frontend login.js script

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,41 +1,64 @@
-  
+
+
   
 
-  // Handler for the login option
-  async function loginFormHandler(event) {
-    
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-    
-    if (email && password) {
-      const response = await fetch('/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-         // Store the token in local storage
-         
-         
-         let data = await response.json();
-         let accessToken = await data.accessToken
-         console.log(accessToken)
-         localStorage.setItem('savedAccesToken', accessToken)
 
+// Handler for the login option
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+  
+  if (email && password) {
+    const response = await fetch('/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
         
-        //document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
+        
+      let data = await response.json();
+      let accessToken = await data.accessToken
+      //console.log(accessToken)
+        // Store the token in local storage
+      
+      localStorage.setItem('savedAccesToken', accessToken)
+
+      
+      //document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
-    
   }
   
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+}
+
+
+// Test function to test token login
+async function checkFormHandler(event) {
+  event.preventDefault();
+
+  //console.log( 'Bearer ' + localStorage.getItem('savedAccesToken'));
+  response = await fetch('/books', {
+    method:'get',
+    headers: {'Authorization': 'Bearer ' + localStorage.getItem('savedAccesToken')}
+  })
+
   
+
+
+  console.log(await response.json())
+
+}
+
+
+
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.check-form').addEventListener('submit', checkFormHandler);
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path');
+const path = require('path'); // for stitching together paths
 const sequelize = require('./config/connection');
 const jwt = require('jsonwebtoken');
 
 
+// server configuration
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.engine('handlebars', exphbs());
+
+app.use(express.json()); // needed to code and decode req and res
+
+// Set the handlebars enging (npm express-handlebars)
+app.engine('handlebars', exphbs()); 
 app.set('view engine', 'handlebars');
 
-
+// enable use of a static folder for client side js
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // ========================
 // Auth test part
@@ -61,9 +66,12 @@ app.post('/login', (req, res) => {
 // middleware functon to authorize token
 function authenticateToken(req,res,next){
     // look in the autorization part of the httprequest header
+    
+  
     const authHeader = req.headers['authorization']
     // dig out the token
     const token = authHeader && authHeader.split(' ')[1]
+    
     // check if there is a token
     if(token == null){
         return res.sendStatus(401)
@@ -83,6 +91,8 @@ function authenticateToken(req,res,next){
 
 // =================================
 
+
+// launch SQL server and app server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
