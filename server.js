0630b561203136bf3e0cf891eeda3a8c67e84570
Bennett Collier <bennettcollier@Bennetts-MacBook-Pro.local>
2021-01-20 09:44:42 -0800

    login styling

--- server.js
+++ server.js
@@ -12,22 +12,23 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.use(express.json()); // needed to code and decode req and res
-
 // Set the handlebars enging (npm express-handlebars)
 app.engine('handlebars', exphbs()); 
 app.set('view engine', 'handlebars');
+
+//importing Helpers function for formatting help. 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // enable use of a static folder for client side js
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.use(controllers);
 //app.use(session);
-
-
 // launch SQL server and app server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
