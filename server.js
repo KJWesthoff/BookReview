3fcf752eb0b8caa8a6dd6c29ceb28e1cc86d16d4
Anuj Chatterjee <anujchatterjee@gmail.com>
2021-01-20 18:30:29 -0800

    fixed the helper functions on the homepage

--- server.js
+++ server.js
@@ -5,21 +5,14 @@ const sequelize = require('./config/connection');
 const controllers = require('./controllers');
 //const session = require('express-session');
 const cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session')  
+const cookieSession = require('cookie-session')  
 
 
 // server configuration
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set the handlebars enging (npm express-handlebars)
-app.engine('handlebars', exphbs()); 
-app.set('view engine', 'handlebars');
 
-//importing Helpers function for formatting help. 
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
 
 
 app.use(cookieSession({
@@ -29,6 +22,18 @@ app.use(cookieSession({
 );
 
 app.use(cookieParser());
+
+
+//importing Helpers function for formatting help. 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+// Set the handlebars enging (npm express-handlebars)
+app.engine('handlebars', hbs.engine); 
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // enable use of a static folder for client side js
@@ -36,9 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(controllers);
 // launch SQL server and app server
-
-
-
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
