65f7de2fe85ff88d34e41edf54b4430e39d71af8
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-01-23 10:00:49 -0800

    adding stars

--- public/javascript/getbooks.js
+++ public/javascript/getbooks.js
@@ -129,41 +129,83 @@ const addBookToUser = async function () {
 
 // Stars
 jQuery(document).ready(function($) {
-  $('.rating_stars span.r').hover(function() {
-              // get hovered value
-              var rating = $(this).data('rating');
-              var value = $(this).data('value');
-              $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
-              highlight_star(value);
-          }, function() {
-              // get hidden field value
-              var rating = $("#rating").val();
-              var value = $("#rating_val").val();
-              $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
-              highlight_star(value);
-          }).click(function() {
-              // Set hidden field value
-              var value = $(this).data('value');
-              $("#rating_val").val(value);
-  
-              var rating = $(this).data('rating');
-              $("#rating").val(rating);
-              
-              highlight_star(value);
-          });
+  $('body').on('mouseover','.rating_stars span.r', function(event) {
+    event.stopImmediatePropagation()
+    
+    //console.log(this)
+    var thiscard = (this.closest(".card"))
+    let title = thiscard.querySelector(".card-title").textContent;
+    let author = thiscard.querySelector(".card-text").textContent;
+    let img_url = thiscard.querySelector("img").getAttribute("src");
+
+
+
+    var rating = $(this).data('rating');
+    var value = $(this).data('value');
+    console.log(title,rating,value)
+    
+    $(this, "rating_stars").parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
+    highlight_star(value);          
+        
+   
+});
+
+
+
+
+
           
           var highlight_star = function(rating) {
-              $('.rating_stars span.s').each(function() {
-                  var low = $(this).data('low');
-                  var high = $(this).data('high');
-                  $(this).removeClass('active-high').removeClass('active-low');
-                  if (rating >= high) $(this).addClass('active-high');
-                  else if (rating == low) $(this).addClass('active-low');
-              });
-          }
-  });
+            $('.rating_stars span.s').each(function() {
+                var low = $(this).data('low');
+                var high = $(this).data('high');
+                $(this).removeClass('active-high').removeClass('active-low');
+                if (rating >= high) $(this).addClass('active-high');
+                else if (rating == low) $(this).addClass('active-low');
+            });
+        }
 
+});
+          
+          
+          
+          /*
+          jQuery(document).ready(function($) {
+$('.rating_stars span.r').hover(function() {
+            // get hovered value
+            var rating = $(this).data('rating');
+            var value = $(this).data('value');
+            $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
+            highlight_star(value);
+        }, function() {
+            // get hidden field value
+            var rating = $("#rating").val();
+            var value = $("#rating_val").val();
+            $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
+            highlight_star(value);
+        }).click(function() {
+            // Set hidden field value
+            var value = $(this).data('value');
+            $("#rating_val").val(value);
 
+            var rating = $(this).data('rating');
+            $("#rating").val(rating);
+            
+            highlight_star(value);
+        });
+        
+        var highlight_star = function(rating) {
+            $('.rating_stars span.s').each(function() {
+                var low = $(this).data('low');
+                var high = $(this).data('high');
+                $(this).removeClass('active-high').removeClass('active-low');
+                if (rating >= high) $(this).addClass('active-high');
+                else if (rating == low) $(this).addClass('active-low');
+            });
+        }
+});
+
+*/
 
 
 
