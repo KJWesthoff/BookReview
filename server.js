9f7486672619ae16dcc11c8b79ef31b2b61df266
KJWesthoff <69325151+KJWesthoff@users.noreply.github.com>
2021-01-23 16:50:23 -0800

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(controllers);
 // launch SQL server and app server
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
 
